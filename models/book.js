a726cd6efae5c425f420fefa3fd855d633433e54
shadrack <shadrackkinsimba@gmail.com>
2025-01-19 12:23:19 +0300

    creating books route/create new

--- models/book.js
+++ models/book.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const path = require('path')
 
 const coverImageBasePath = 'uploads/bookCovers'
 
@@ -36,6 +37,12 @@ const bookSchema = new mongoose.Schema({
 
 
 
+
+})
+bookSchema.virtual('coverImagePath').get(function(){
+    if (this.coverImageName != null) {
+    return path.join('/', coverImageBasePath, this.coverImageName)
+  }
 
 })
 module.exports =mongoose.model('Book', bookSchema)
